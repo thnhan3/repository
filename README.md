df7193af9529bfb0340c60e31325ae1e4c43da45
thnhan3 <huunhantran10@gmail.com>
2024-10-07 23:00:27 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Demo portfolio with github page
+# portfolio
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Xin chao</h1>
+      <h2>Deploy with github action</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus, enim?
+        Voluptates, natus rem. Eos at ullam, a nisi culpa error minima accusamus
+        magnam veritatis eius illum assumenda ex pariatur quidem.
+      </p>
+    </>
+  );
+}
+
+export default App;
